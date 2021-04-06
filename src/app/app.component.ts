afaa76d7be54d91d5fe86988323d118dc30440b7
Priya Bharmbe <priyabharambe70@gmail.com>
2021-04-06 20:07:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'APISubObject';
+
+  apiUrl = 'http://jsonplaceholder.typicode.com/users';
+
+  apiData: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  ngOnInit() {
+    this.http.get(this.apiUrl).subscribe((data) => {
+      console.warn(data)
+      this.apiData = data;
+    })
+  }
 }
